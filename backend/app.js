f86a36d3aaf06c4da6f7d14fe92e72f126069510
Your Name <you@example.com>
2026-04-17 12:27:31 +0300

    feat/CRM-10: Création des règles métiers en une seule fichier

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,111 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.send("SmartCRM Backend is running");
+});
+
+app.post("/api/lead_score", (req, res) => {
+  let score = 0;
+  let tag = "";
+
+  const data = req.body;
+
+  if (data.emailOpened) {
+    score = score + 10;
+
+    if (data.emailClicked) {
+      score = score + 20;
+
+      if (data.jobTitle) {
+        if (data.jobTitle === "CEO" || data.jobTitle === "Directeur") {
+          score = score + 50;
+
+          if (data.lastActivityDays > 30) {
+            score = score - 30;
+
+            if (score < 0) {
+              score = 0;
+            } else {
+              if (score > 80) {
+                tag = "HOT LEAD";
+              } else {
+                tag = "NORMAL";
+              }
+            }
+          } else {
+            if (score > 80) {
+              tag = "HOT LEAD";
+            } else {
+              tag = "NORMAL";
+            }
+          }
+        } else {
+          if (data.lastActivityDays > 30) {
+            score = score - 30;
+
+            if (score < 0) {
+              score = 0;
+            } else {
+              if (score > 80) {
+                tag = "HOT LEAD";
+              } else {
+                tag = "NORMAL";
+              }
+            }
+          } else {
+            if (score > 80) {
+              tag = "HOT LEAD";
+            } else {
+              tag = "NORMAL";
+            }
+          }
+        }
+      }
+    } else {
+      if (data.lastActivityDays > 30) {
+        score = score - 30;
+
+        if (score < 0) {
+          score = 0;
+        } else {
+          if (score > 80) {
+            tag = "HOT LEAD";
+          } else {
+            tag = "NORMAL";
+          }
+        }
+      }
+    }
+  } else {
+    if (data.lastActivityDays > 30) {
+      score = score - 30;
+
+      if (score < 0) {
+        score = 0;
+      } else {
+        if (score > 80) {
+          tag = "HOT LEAD";
+        } else {
+          tag = "NORMAL";
+        }
+      }
+    }
+  }
+  console.log(
+    `Score calculé pour ${data.email || "le prospect"}: ${score}, Tag: ${tag}`
+  );
+  //   console.log(JSON.stringify(data));
+  res.json({
+    score: score,
+    tag: tag,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Démarrage de serveur sur le port : ${PORT}`);
+});
