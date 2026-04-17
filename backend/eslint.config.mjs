7878def758f9d30cbd353ab599ad16bfbf710f79
ick <hajaritiana.ick@gmail.com>
2026-04-17 13:35:32 +0300

    feat/CRM-7: Ajout de la compléxité

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -3,6 +3,9 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.{js,mjs,cjs}"],rules: {
+      ...js.configs.recommended.rules,
+      "complexity": ["error", 5] // Ton seuil de complexité cyclomatique
+    }, plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
 ]);
